f8181bf7aa43de6137cae99b9c6ceb08f925f38e
PLR <51248199+plredmond@users.noreply.github.com>
2013-10-03 00:58:01 -0400

    simplified and matched cpp functionality

---
+++ Tut_03_OpenGLs_Moving_Triangle/hs/vertPositionOffset.hs
@@ -0,0 +1,73 @@
+import System.Environment (getProgName)
+import qualified Graphics.UI.GLUT as GLUT
+import Graphics.GPipe
+import Data.Vec as V
+import Prelude as P
+
+main :: IO ()
+main = do
+    GLUT.getArgsAndInitialize
+    n <- getProgName
+    newWindow
+        n -- window title
+        (300:.200:.()) -- desired window position
+        (500:.500:.()) -- desired window size
+        displayIO
+        initWindow
+    GLUT.mainLoop
+
+initWindow :: GLUT.Window -> IO ()
+initWindow w = do
+    GLUT.idleCallback GLUT.$= Just (GLUT.postRedisplay $ Just w)
+    GLUT.keyboardMouseCallback GLUT.$= Just onKeyMouse
+    where
+        onKeyMouse :: GLUT.Key -> GLUT.KeyState -> GLUT.Modifiers -> GLUT.Position -> IO ()
+        onKeyMouse (GLUT.Char '\ESC') GLUT.Down _ _ = do GLUT.leaveMainLoop
+        onKeyMouse _ _ _ _ = do return ()
+
+displayIO :: Vec2 Int -> IO (FrameBuffer RGBAFormat () ())
+displayIO size = do
+    milliseconds <- GLUT.get GLUT.elapsedTime
+    return $ display size (fromIntegral milliseconds / 1000)
+
+display :: Vec2 Int -> Float -> FrameBuffer RGBAFormat () ()
+display size sec = draw fragments cleared
+    where
+        xyoffset = V.append (computePositionOffsets sec) (vec 0)
+        -- draw -- curry blending mode and boolean color mask onto paintColor
+        draw :: FragmentStream (Color RGBAFormat (Fragment Float))
+                -> FrameBuffer RGBAFormat () ()
+                -> FrameBuffer RGBAFormat () ()
+        draw = paintColor NoBlending (RGBA (vec True) True)
+        -- cleared -- a solid color framebuffer
+        cleared :: FrameBuffer RGBAFormat () ()
+        cleared = newFrameBufferColor $ RGBA (vec 0) 1
+        -- fragment stream
+        fragments :: FragmentStream (Color RGBAFormat (Fragment Float))
+        fragments = fmap fs
+                  $ rasterizeBack
+                  $ fmap (vs $ toGPU xyoffset)
+                  stream
+
+stream :: PrimitiveStream Triangle (Vec4 (Vertex Float))
+stream = toGPUStream TriangleList
+    [ ( 0.25):.( 0.25):.0:.1:.()
+    , ( 0.25):.(-0.25):.0:.1:.()
+    , (-0.25):.(-0.25):.0:.1:.()
+    ]
+
+computePositionOffsets :: Float -> Vec2 Float
+computePositionOffsets elapsedTime = (0.5 * cos (currTimeThroughLoop * scale)) :.
+                                     (0.5 * sin (currTimeThroughLoop * scale)) :. ()
+    where
+        loopDuration = 5
+        scale = pi * 2 / loopDuration
+        currTimeThroughLoop = mod' elapsedTime loopDuration
+
+vs :: Vec4 (Vertex Float) -> Vec4 (Vertex Float) -> (Vec4 (Vertex Float), ())
+vs xyoffset pos = (xyoffset + pos, ())
+
+fs :: () -> Color RGBAFormat (Fragment Float)
+fs _ = RGBA (vec 1) 1
+
+-- eof
