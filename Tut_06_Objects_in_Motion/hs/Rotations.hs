7fb9ba639ab171a6d0337703107a8b904538b43a
PLR <51248199+plredmond@users.noreply.github.com>
2013-10-07 20:37:27 -0400

    simplified and matched cpp functionality

--- Tut_06_Objects_in_Motion/hs/Rotations.hs
+++ Tut_06_Objects_in_Motion/hs/Rotations.hs
@@ -3,53 +3,90 @@ import qualified Graphics.UI.GLUT as GLUT
 import Graphics.GPipe
 import Data.Vec as V
 import Prelude as P
-import Data.IORef
--- local
---import Typenames
-import RenderState
-import Tetrahedron
-import Util
 
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- Main routines
+import Tetrahedron
+import RenderState
+import AnimUtils
 
 main :: IO ()
 main = do
     GLUT.getArgsAndInitialize
     n <- getProgName
-    newWindow n (vec 8) (768:.512:.()) displayIO prepWindow
+    newWindow
+        n -- window title
+        (300:.200:.()) -- desired window position
+        (500:.500:.()) -- desired window size
+        displayIO
+        initWindow
     GLUT.mainLoop
 
--- Set up the window before entering the GLUT mainloop
--- IO: Set GLUT state variables
-prepWindow :: GLUT.Window -> IO ()
-prepWindow w = do
+initWindow :: GLUT.Window -> IO ()
+initWindow w = do
     GLUT.idleCallback GLUT.$= Just (GLUT.postRedisplay $ Just w)
     GLUT.keyboardMouseCallback GLUT.$= Just onKeyMouse
-    -- depthClamp: How should zNear & zFar be enforced?
-    --     Enabled: Clamp the depth of fragments (squish against glass).
-    --     Disabled: Clip triangles (cut models).
     GLUT.depthClamp GLUT.$= GLUT.Enabled
     where
-        -- Handle a keyboard or mouse event
-        -- IO: leave the glut mainloop on some key events
         onKeyMouse :: GLUT.Key -> GLUT.KeyState -> GLUT.Modifiers -> GLUT.Position -> IO ()
         onKeyMouse (GLUT.Char '\ESC') GLUT.Down _ _ = do GLUT.leaveMainLoop
-        onKeyMouse _             _         _ _ = do return ()
+        onKeyMouse _ _ _ _ = do return ()
+
+displayIO :: Vec2 Int -> IO (FrameBuffer RGBAFormat DepthFormat ())
+displayIO size = do
+    rs <- mkRenderState size
+    return $ display rs
+
+display :: RenderState Float -> FrameBuffer RGBAFormat DepthFormat ()
+display rs = P.foldl (flip draw) cleared -- draw in-order onto the framebuffer
+           $ P.map mkFragments 
+           $ zip modelToCameraMatrices (repeat cameraToClipMatrix)
+    where
+        -- draw -- curry blending mode and boolean color mask onto paintColor
+        draw :: FragmentStream (Color RGBAFormat (Fragment Float))
+                -> FrameBuffer RGBAFormat DepthFormat ()
+                -> FrameBuffer RGBAFormat DepthFormat ()
+        draw = paintColorRastDepth Less True NoBlending (RGBA (vec True) True)
+        -- cleared -- a solid color framebuffer
+        cleared :: FrameBuffer RGBAFormat DepthFormat ()
+        cleared = newFrameBufferColorDepth (RGBA (vec 0) 1) 1
+        -- these matrices are uniforms calculated every frame
+        cameraToClipMatrix = toGPU $ perspective 1 45 (45 * pi / 180) (rsAspectRatio rs)
+        modelToCameraMatrices = P.map toGPU
+                              $ P.map ($ rsSeconds rs)
+                              $ P.map (constructMatrix $)
+                              $ g_instanceList
+
+mkFragments :: (Mat44 (Vertex Float), Mat44 (Vertex Float)) -> FragmentStream (Color RGBAFormat (Fragment Float))
+mkFragments mattup = fmap fs
+                   $ rasterizeBack
+                   $ fmap (vs mattup)
+                   $ fmap vs0 tetrahedron -- imported immutable stream
+
+-- Make the stream use Vec4 for pos and col.
+vs0 :: (Vec3 (Vertex Float), Vec3 (Vertex Float)) -> (Vec4 (Vertex Float), Vec4 (Vertex Float))
+vs0 (pos3, rgb) = (homPoint pos3, snoc rgb 1)
+
+-- Offset the position. Perform projection using the provided matrix.
+vs :: (Mat44 (Vertex Float), Mat44 (Vertex Float)) -> (Vec4 (Vertex Float), Vec4 (Vertex Float)) -> (Vec4 (Vertex Float), Vec4 (Vertex Float))
+vs (modelToCameraMatrix, cameraToClipMatrix) (pos, col) = (clipPos, col)
+    where
+        camPos = multmv modelToCameraMatrix pos
+        clipPos = multmv cameraToClipMatrix camPos
+
+fs :: Vec4 (Fragment Float) -> Color RGBAFormat (Fragment Float)
+fs col = RGBA (V.take n3 col) (V.last col)
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- Types of translated object instances
+-- Types of rotated object instances
 
 type RotationFunc = Float -> Mat44 Float
-data Instance = Instance { instCalcRotation :: RotationFunc
-                         , instOffset :: Vec3 Float
-                         }
+type Instance = (RotationFunc, Vec3 Float)
+
 -- Produce a rotation and translation matrix.
 constructMatrix :: Instance -> Float -> Mat44 Float
-constructMatrix inst elapsedTime = ro_mat
+constructMatrix (calcRotation, offset3) elapsedTime = ro_mat
     where
-        r_mat = instCalcRotation inst $ elapsedTime :: Mat44 Float
-        offset = snoc (instOffset inst) 1 :: Vec4 Float
+        r_mat = calcRotation elapsedTime
+        offset = homPoint offset3
 --      [[ r, r, r,ox],
 --       [ r, r, r,oy],
 --       [ r, r, r,oz],
@@ -58,12 +95,12 @@ constructMatrix inst elapsedTime = ro_mat
 
 -- A list of instances which produce translation/scaling matrices
 g_instanceList :: [Instance]
-g_instanceList = [ Instance nullRotation (( 0):.( 0):.(-25):.())
-                 , Instance rotateX      ((-5):.(-5):.(-25):.())
-                 , Instance rotateY      ((-5):.( 5):.(-25):.())
-                 , Instance rotateZ      (( 5):.( 5):.(-25):.())
-                 , Instance rotateAxis   (( 5):.(-5):.(-25):.())
-                 , Instance rotateAxis2  (( 8):.( 0):.(-25):.())
+g_instanceList = [ (nullRotation, ( 0):.( 0):.(-25):.())
+                 , (rotateX,      (-5):.(-5):.(-25):.())
+                 , (rotateY,      (-5):.( 5):.(-25):.())
+                 , (rotateZ,      ( 5):.( 5):.(-25):.())
+                 , (rotateAxis,   ( 5):.(-5):.(-25):.())
+                 --, (rotateAxis2,  ( 8):.( 0):.(-25):.())
                  ]
 
 nullRotation :: RotationFunc
@@ -99,73 +136,4 @@ computeAngleRad elapsedTime loopDuration = curTimeThroughLoop * scale
         curTimeThroughLoop = mod' elapsedTime loopDuration
         scale = pi * 2 / loopDuration
 
--- Compute a lerp value [0, 1) which ranges from 0 to 1 to 0 every
--- loopDuration seconds of elapsedTime.
-calcLerpFactor :: Float -> Float -> Float
-calcLerpFactor elapsedTime loopDuration = 2 * zeroToHalf
-    where
-        loopFraction = mod' elapsedTime loopDuration / loopDuration
-        zeroToHalf = if loopFraction > 0.5 then 1 - loopFraction else loopFraction
-
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- Display function
-
-type ColorDepthBuffer = FrameBuffer RGBFormat DepthFormat ()
-
--- An empty framebuffer with depth and color
-cleared :: ColorDepthBuffer
-cleared = newFrameBufferColorDepth c d
-    where
-        c = RGB (vec 0)
-        d = 1
-
-paint = paintColorRastDepth Lequal True NoBlending (RGB $ vec True)
-
--- Produce a framebuffer of animation, provided the size of the window
--- IO: Call makeRenderState
-displayIO :: Vec2 Int -> IO ColorDepthBuffer
-displayIO size = do
-    rs <- makeRenderState size
-    return $ display rs
-
-display :: RenderState Float -> ColorDepthBuffer
---display rs = cleared
-display rs = P.foldl (flip paint) cleared
-        $ P.map (fmap fragmentShader)
-        $ P.map rasterizeBack
-        $ P.map (\mo2ca -> fmap (vertexShader mo2ca cameraToClipMatrix) tetrahedron)
-        $ modelToCameraMatrices
-    where 
-        modelToCameraMatrices :: [Vf44]
-        modelToCameraMatrices = P.map toGPU
-                              $ P.map ($ rsSeconds rs)
-                              $ P.map (constructMatrix $)
-                              $ g_instanceList
-        cameraToClipMatrix :: Vf44
-        cameraToClipMatrix = toGPU $ let zNear = 1; zFar = 61; fov = 45
-            in perspective zNear zFar (degToRad fov) (rsAspectRatio rs)
-
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- Shader functions
-
-type Vf  = Vertex Float
-type Vf3 = Vec3 Vf
-type Vf4 = Vec4 Vf
-type Vf44 = Mat44 Vf
-
-type Ff  = Fragment Float
-type Ff3 = Vec3 Ff
-type RGB = Color RGBFormat Ff
-
-vertexShader :: Vf44 -> Vf44 -> (Vf3, Vf3) -> (Vf4, Vf3)
-vertexShader modelToCameraMatrix cameraToClipMatrix (position3, color) = (gl_Position, color)
-    where
-        position = snoc position3 1 -- add w coord
-        cameraPos = multmv modelToCameraMatrix position
-        gl_Position = multmv cameraToClipMatrix cameraPos
-
-fragmentShader :: Ff3 -> RGB
-fragmentShader color = RGB color
-
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- EOF
+-- eof
