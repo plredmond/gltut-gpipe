6d632ee41a7e803b15efa4933cf933a71619dac2
PLR <51248199+plredmond@users.noreply.github.com>
2013-12-16 16:56:18 -0500

    finished mprim extraction to loaders

--- Tut_07_World_in_Motion/hs/Lib/Mesh/MPrim.hs
+++ Tut_07_World_in_Motion/hs/Lib/Mesh/MPrim.hs
@@ -7,6 +7,9 @@ import Text.XML.Light hiding (Line)
 import Text.Printf (printf)
 import Text.Read (readEither)
 
+import Data.Maybe (catMaybes)
+import Data.Either (partitionEithers)
+
 -- local
 import Lib.Mesh.XML
 import Lib.Mesh.Util
@@ -80,5 +83,50 @@ readMPrimCmd "line-strip" = Right MPrimLineStrip
 readMPrimCmd "line-loop"  = Right MPrimLineLoop
 readMPrimCmd "points"     = Right MPrimPointList
 readMPrimCmd s            = Left $ printf "String \"%s\" doesn't look like a primitive command" s
+
+
+data MPrimToGPU a = MPrimToGPUTriangle ([CPU a] -> PrimitiveStream Triangle a)
+                  | MPrimToGPULine     ([CPU a] -> PrimitiveStream Line     a)
+                  | MPrimToGPUPoint    ([CPU a] -> PrimitiveStream Point    a)
+
+extractMPrim' :: (VertexInput a) => MPrim -> Either String (MPrimToGPU a)
+extractMPrim' mp = case mpc of
+        MPrimTriangleList  -> Right . MPrimToGPUTriangle $ toLoader mp TriangleList
+        MPrimTriangleStrip -> Right . MPrimToGPUTriangle $ toLoader mp TriangleStrip
+        MPrimTriangleFan   -> Right . MPrimToGPUTriangle $ toLoader mp TriangleFan
+        MPrimLineList      -> Right . MPrimToGPULine     $ toLoader mp LineList
+        MPrimLineStrip     -> Right . MPrimToGPULine     $ toLoader mp LineStrip
+        MPrimLineLoop      -> Left "GPipe does not support LineLoop"
+        MPrimPointList     -> Right . MPrimToGPUPoint    $ toLoader mp PointList
+    where
+        mpc :: MPrimCmd
+        mpc = case mp of MPrimIndex x _ -> x
+                         MPrimArray x _ _ -> x
+        toLoader :: (Primitive p, VertexInput a) => MPrim -> p -> [CPU a] -> PrimitiveStream p a
+        toLoader (MPrimIndex _ cis  ) = let is = map fromIntegral cis
+                                         in \p xs -> toIndexedGPUStream p xs is
+        toLoader (MPrimArray _ ci cn) = let i = fromIntegral ci
+                                            n = fromIntegral cn
+                                         in \p xs -> toGPUStream p (take n . drop i $ xs)
+
+type MPrimArbToGPU a = ( Maybe ([CPU a] -> PrimitiveStream Triangle a)
+                       , Maybe ([CPU a] -> PrimitiveStream Line     a)
+                       , Maybe ([CPU a] -> PrimitiveStream Point    a)
+                       )
+
+ambiguate :: MPrimToGPU a -> MPrimArbToGPU a
+ambiguate (MPrimToGPUTriangle f) = (Just f, Nothing, Nothing)
+ambiguate (MPrimToGPULine     f) = (Nothing, Just f, Nothing)
+ambiguate (MPrimToGPUPoint    f) = (Nothing, Nothing, Just f)
+
+extractMPrims :: (VertexInput a)
+              => [MPrim] -> Either String ( [[CPU a] -> PrimitiveStream Triangle a]
+                                          , [[CPU a] -> PrimitiveStream Line     a]
+                                          , [[CPU a] -> PrimitiveStream Point    a]
+                                          )
+extractMPrims mps = do
+   fs <- mapM extractMPrim' mps
+   let (as, bs, cs) = unzip3 . map ambiguate $ fs
+   return (catMaybes as, catMaybes bs, catMaybes cs)
 
 -- eof
