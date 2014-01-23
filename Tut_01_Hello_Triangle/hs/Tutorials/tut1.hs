995059af9dbacb089bf9ca248b11c3a6eded50dc
PLR <51248199+plredmond@users.noreply.github.com>
2014-01-23 16:53:07 -0500

    removed extraneous types

--- Tut_01_Hello_Triangle/hs/Tutorials/tut1.hs
+++ Tut_01_Hello_Triangle/hs/Tutorials/tut1.hs
@@ -2,7 +2,7 @@ import qualified Graphics.GLTut.Framework as Framework
 import qualified Graphics.UI.GLUT as GLUT
 
 import Graphics.GPipe
-import Data.Vec
+import Data.Vec as V
 
 main :: IO ()
 main = Framework.main keyboard displayIO (\_ -> return ())
@@ -18,16 +18,8 @@ displayIO size = do
 display :: Vec2 Int -> FrameBuffer RGBFormat () ()
 display _ = draw fragments cleared
     where
-        -- draw -- curry blending mode and boolean color mask onto paintColor
-        draw :: FragmentStream (Color RGBFormat (Fragment Float))
-                -> FrameBuffer RGBFormat () ()
-                -> FrameBuffer RGBFormat () ()
         draw = paintColor NoBlending (RGB $ vec True)
-        -- cleared -- a solid color framebuffer
-        cleared :: FrameBuffer RGBFormat () ()
         cleared = newFrameBufferColor (RGB $ vec 0)
-        -- fragment stream
-        fragments :: FragmentStream (Color RGBFormat (Fragment Float))
         fragments = fmap fs
                   $ rasterizeBack
                   $ fmap vs
