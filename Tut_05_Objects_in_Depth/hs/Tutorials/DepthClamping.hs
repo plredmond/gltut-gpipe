ddec0c6785fa51bf49c612837a63f43ef8d39cab
PLR <51248199+plredmond@users.noreply.github.com>
2014-01-24 15:44:21 -0500

    reduced differences between tutorials; converted to use Framework.main; changed rgba to rgb; removed extraneous types

--- Tut_05_Objects_in_Depth/hs/Tutorials/DepthClamping.hs
+++ Tut_05_Objects_in_Depth/hs/Tutorials/DepthClamping.hs
@@ -1,97 +1,89 @@
-import System.Environment (getArgs, getProgName)
+import qualified Graphics.GLTut.Framework as Framework
+import qualified Graphics.GLTut.Perspective as Perspective
+import qualified Graphics.GLTut.VecFile as VecFile
+import qualified Graphics.GLTut.Easing as Easing
 import qualified Graphics.UI.GLUT as GLUT
+import qualified Paths_gltut_tut05 as Paths
+
+import Data.Monoid (mconcat)
+
 import Graphics.GPipe
 import Data.Vec as V
 import Prelude as P
 
-import Paths_gltut05
-
-import Lib.Load
-import qualified Lib.Perspective
-import Lib.TimeFun
-
-type Stream = PrimitiveStream Triangle (Vec4 (Vertex Float), Vec4 (Vertex Float))
-type Object = (Stream, Vec4 (Vertex Float))
-
-indexes =
-    [ 0, 2, 1
-    , 3, 2, 0
-    , 4, 5, 6
-    , 6, 7, 4
-    , 8, 9, 10
-    , 11, 13, 12
-    , 14, 16, 15
-    , 17, 16, 14 ]
+type Object = ( PrimitiveStream Triangle (Vec4 (Vertex Float), Vec4 (Vertex Float))
+              , Vec4 (Vertex Float)
+              )
 
 main :: IO ()
 main = do
-    GLUT.getArgsAndInitialize
-    n <- getProgName
-    f_horiz <- getDataFileName "obj-horiz.vec4"
-    f_vert  <- getDataFileName "obj-vert.vec4"
-    s_horiz <- readFile f_horiz
-    s_vert  <- readFile f_vert
-    newWindow
-        n -- window title
-        (300:.200:.()) -- desired window position
-        (500:.500:.()) -- desired window size
-        (displayIO [ (readIndexedStream s_horiz indexes, toGPU (0:.0:.( 0.5):.0:.()))
-                   , (readIndexedStream s_vert  indexes, toGPU (0:.0:.(-1  ):.0:.()))
-                   ])
-        initWindow
-    GLUT.mainLoop
-
-initWindow :: GLUT.Window -> IO ()
-initWindow w = do
-    GLUT.idleCallback GLUT.$= Just (GLUT.postRedisplay $ Just w)
-    GLUT.keyboardMouseCallback GLUT.$= Just onKeyMouse
-    GLUT.depthClamp GLUT.$= GLUT.Enabled
+    dat_h <- Paths.getDataFileName "obj-horiz.vec4" >>= readFile
+    dat_v <- Paths.getDataFileName "obj-vert.vec4"  >>= readFile
+    Framework.main keyboard
+                   (displayIO [ (VecFile.readIndexedStream dat_h indexes, toGPU (0:.0:.0.5:.0:.()))
+                              , (VecFile.readIndexedStream dat_v indexes, toGPU (0:.0:.(-1):.0:.()))
+                              ])
+                   initialize
     where
-        onKeyMouse :: GLUT.Key -> GLUT.KeyState -> GLUT.Modifiers -> GLUT.Position -> IO ()
-        onKeyMouse (GLUT.Char '\ESC') GLUT.Down _ _ = do GLUT.leaveMainLoop
-        onKeyMouse (GLUT.Char ' '   ) GLUT.Down _ _ = do
-            x <- GLUT.get GLUT.depthClamp
-            GLUT.depthClamp GLUT.$= if x == GLUT.Disabled then GLUT.Enabled else GLUT.Disabled
-        onKeyMouse _ _ _ _ = do return ()
+        indexes = [ 0, 2, 1
+                  , 3, 2, 0
+                  , 4, 5, 6
+                  , 6, 7, 4
+                  , 8, 9, 10
+                  , 11, 13, 12
+                  , 14, 16, 15
+                  , 17, 16, 14 ]
 
-displayIO :: [Object] -> Vec2 Int -> IO (FrameBuffer RGBAFormat DepthFormat ())
-displayIO objs size = do
+initialize :: GLUT.Window -> IO ()
+initialize w = do GLUT.idleCallback GLUT.$= (Just . GLUT.postRedisplay . Just $ w)
+                  GLUT.depthClamp GLUT.$= GLUT.Enabled
+
+keyboard :: Char -> GLUT.Position -> IO ()
+keyboard '\ESC' _ = do GLUT.leaveMainLoop
+keyboard ' '    _ = do x <- GLUT.get GLUT.depthClamp
+                       GLUT.depthClamp GLUT.$= if x == GLUT.Disabled then GLUT.Enabled else GLUT.Disabled
+keyboard _      _ = do return ()
+
+displayIO :: [Object] -> Vec2 Int -> IO (FrameBuffer RGBFormat DepthFormat ())
+displayIO objects size = do
     milliseconds <- GLUT.get GLUT.elapsedTime
-    return $ display objs size (fromIntegral milliseconds / 1000)
+    return $ display objects size (fromIntegral milliseconds / 1000)
 
-display :: [Object] -> Vec2 Int -> Float -> FrameBuffer RGBAFormat DepthFormat ()
-display objs size sec = P.foldl (flip draw) cleared -- draw in-order onto the framebuffer
-                      $ P.map (mkFragments matrix) objs'
+display :: [Object] -> Vec2 Int -> Float -> FrameBuffer RGBFormat DepthFormat ()
+display objects size sec = draw fragments cleared
     where
-        -- draw -- curry blending mode and boolean color mask onto paintColor
-        draw :: FragmentStream (Color RGBAFormat (Fragment Float))
-                -> FrameBuffer RGBAFormat DepthFormat ()
-                -> FrameBuffer RGBAFormat DepthFormat ()
-        draw = paintColorRastDepth Less True NoBlending (RGBA (vec True) True)
-        -- cleared -- a solid color framebuffer
-        cleared :: FrameBuffer RGBAFormat DepthFormat ()
-        cleared = newFrameBufferColorDepth (RGBA (vec 0) 1) 1
-        -- matrix is a uniform calculated every frame
-        matrix = toGPU $ Lib.Perspective.m_ar 1 1 3 (V.map fromIntegral size)
-        -- offset the first thing
-        o = toGPU $ (1.5) * computeCycle 5 sec
-        objs' = let first:[(s, (x:.y:. z   :.w:.()))] = objs
-                 in first:[(s, (x:.y:.(z+o):.w:.()))] -- Minor deviation from tutorial: We move the first thing forward and backward to demonstrate the depth
+        draw = paintColorRastDepth Less True NoBlending (RGB $ vec True)
+        cleared = newFrameBufferColorDepth (RGB $ vec 0) 1
+        fragments = mconcat
+                  . P.map (mkFragments matrix)
+                  $ objects'
+        -- Deviation from tutorial: We vary the z of the first object's offset to demonstrate the problem with depth.
+        objects' = let (stream, offset        ):rest = objects
+                    in (stream, offset + tweak):rest
+        -- constant uniforms, calculated once
+        -- variable uniforms, calculated every frame
+        tweak :: Vec4 (Vertex Float)
+        tweak = let z = (-1.3) * Easing.computeCycle 5 sec
+                 in toGPU (0:.0:.z:.0:.())
+        matrix = toGPU $ Perspective.m_ar 1 1 3 (V.map fromIntegral size)
 
-mkFragments :: Mat44 (Vertex Float) -> Object -> FragmentStream (Color RGBAFormat (Fragment Float))
+mkFragments :: Mat44 (Vertex Float) -> Object -> FragmentStream (Color RGBFormat (Fragment Float))
 mkFragments matrix (stream, offset) = fmap fs
                                     $ rasterizeBack
                                     $ fmap (vs offset matrix)
                                     stream
 
 -- Offset the position. Perform projection using the provided matrix.
-vs :: Vec4 (Vertex Float) -> Mat44 (Vertex Float) -> (Vec4 (Vertex Float), Vec4 (Vertex Float)) -> (Vec4 (Vertex Float), Vec4 (Vertex Float))
+vs  :: Vec4 (Vertex Float)
+    -> Mat44 (Vertex Float)
+    -> (Vec4 (Vertex Float), Vec4 (Vertex Float))
+    -> (Vec4 (Vertex Float), Vec4 (Vertex Float))
 vs offset mat (pos, col) = (clipPos, col)
     where
         cameraPos = pos + offset
         clipPos = multmv mat cameraPos
 
-fs :: Vec4 (Fragment Float) -> Color RGBAFormat (Fragment Float)
-fs col = RGBA (V.take n3 col) (V.last col)
+fs :: Vec4 (Fragment Float) -> Color RGBFormat (Fragment Float)
+fs = RGB . V.take n3
 
 -- eof
