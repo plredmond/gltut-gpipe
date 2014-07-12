1d0f79baaf13fbe3ad56bbc06b03f7c97ed30349
PLR <51248199+plredmond@users.noreply.github.com>
2014-07-12 02:09:13 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,25 +4,15 @@ mkdir gltut
 cd gltut
 
 git clone https://github.com/tobbebex/GPipe.git
-pushd GPipe
-  git checkout 673aad415e3e3fbf228a302ca14b5f0614d90d6e # GPipe 1.4.3
-  cabal sandbox init
-  cabal install
-popd
-
 git clone https://github.com/plredmond/gltut_haskell-gpipe.git
-pushd gltut_haskell-gpipe
-  pushd framework/hs
-    cabal sandbox init
-    cabal sandbox add-source --snapshot ../../../GPipe
+
+cabal sandbox init --sandbox pkg
+cabal sandbox add-source GPipe
+cabal sandbox add-source gltut_haskell-gpipe/framework/hs
+
+for N in 01 02 03 04 05 06; do
+  pushd gltut_haskell-gpipe/Tut_${N?}*/hs
+    cabal sandbox init --sandbox ../../../pkg
     cabal install
   popd
-  for N in 01 02 03 04 05 06; do
-    pushd Tut_${N?}*/hs
-      cabal sandbox init
-      cabal sandbox add-source --snapshot ../../../GPipe
-      cabal sandbox add-source --snapshot ../../framework/hs
-      cabal install
-    popd
-  done
-popd
+done
