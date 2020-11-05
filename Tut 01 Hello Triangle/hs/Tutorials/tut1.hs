4a67e71240eb036b9f6f1e297ca65fe2f7ece10a
PLR <51248199+plredmond@users.noreply.github.com>
2020-11-04 23:18:38 -0800

    nixify gltut and rewrite the first tutorial (without porting framework)

--- Tut 01 Hello Triangle/hs/Tutorials/tut1.hs
+++ Tut 01 Hello Triangle/hs/Tutorials/tut1.hs
@@ -1,44 +1,86 @@
-import qualified Graphics.GLTut.Framework as Framework
-import qualified Graphics.UI.GLUT as GLUT
+{-# LANGUAGE LambdaCase, NegativeLiterals #-} -- syntax niceties
+{-# LANGUAGE TypeFamilies #-} -- gpipe requirements
 
-import Graphics.GPipe
-import Data.Vec as V
+import Control.Monad.IO.Class (liftIO)
+import System.Environment (getProgName)
+import qualified Control.Concurrent.MVar as MVar
+
+import Graphics.GPipe -- unqualified
+import Graphics.GPipe.Context.GLFW (Handle)
+import qualified Graphics.GPipe.Context.GLFW as GLFW
 
 main :: IO ()
-main = Framework.main keyboard displayIO (\_ -> return ())
+main = runContextT GLFW.defaultHandleConfig $ do
+    -- make a window
+    win <- newWindow (WindowFormatColor RGBA8) . GLFW.defaultWindowConfig =<< liftIO getProgName
+    -- hook up to receive ESC key and window-close events
+    close <- liftIO $ MVar.newEmptyMVar
+    _ <- GLFW.setWindowCloseCallback win . Just $
+        MVar.tryPutMVar close "window closed" >> return ()
+    _ <- GLFW.setKeyCallback win . Just $ \k _ ks _ -> case (k, ks) of
+        (GLFW.Key'Escape, GLFW.KeyState'Pressed) -> MVar.tryPutMVar close "escape key" >> return ()
+        _ -> return ()
+    -- initializeProgram
+    prog <- compileShader shaderCode
+    buff <- initializeVertexBuffer
+    -- framework
+    loop close win buff prog
+  where
+    loop close win buff prog = (liftIO $ MVar.tryReadMVar close) >>= \case
+        Just msg -> liftIO . putStrLn $ "stopping because: " ++ msg
+        Nothing -> display win buff prog >> loop close win buff prog
 
--- Handle keyboard events.
-keyboard :: Char -> GLUT.Position -> IO ()
-keyboard '\ESC' _ = do GLUT.leaveMainLoop
-keyboard _      _ = do return ()
+initializeVertexBuffer :: ContextT Handle os IO (Buffer os (B4 Float))
+initializeVertexBuffer = do
+    positionBufferObject <- newBuffer $ length vertexPositions
+    writeBuffer positionBufferObject 0 vertexPositions
+    return positionBufferObject
+  where
+    vertexPositions =
+        [ V4  0.75  0.75 0 1
+        , V4  0.75 -0.75 0 1
+        , V4 -0.75 -0.75 0 1
+        ]
 
--- Perform IO on behalf of display. Call display to produce the framebuffer.
-displayIO :: Vec2 Int -> IO (FrameBuffer RGBFormat () ())
-displayIO size = do
-    return $ display size
+-- | Rather heavyweight abstraction of the shader environment. Contains the
+-- minimum number of extraction functions for GPipe. If you don't want to use
+-- an environment like this, you could hardcode these values with 'const' into
+-- the shader code.
+data ShaderEnv os prim vert cfmt dfmt = ShaderEnv
+   { getPrimArr :: PrimitiveArray prim vert
+   , getRastOpt :: (Side, ViewPort, DepthRange)
+   , getDrawOpt :: (Window os cfmt dfmt, ContextColorOption cfmt)
+   }
 
--- Combine scene elements on a framebuffer.
-display :: Vec2 Int -> FrameBuffer RGBFormat () ()
-display _ = draw fragments cleared
-    where
-        draw = paintColor NoBlending (RGB $ vec True)
-        cleared = newFrameBufferColor (RGB $ vec 0)
-        fragments = fmap fs
-                  $ rasterizeBack
-                  $ fmap vs
-                  stream
+-- | Somewhat minimal shader which gets the (single) vertex array as a
+-- primitive stream, maps a noop vertex-shader over it, rasterizes to a
+-- fragment stream, maps the noop fragment-shader over it, and draws to the
+-- window specified by the environment.
+shaderCode :: Shader os (ShaderEnv os prim (B4 Float) RGBAFloat dfmt) ()
+shaderCode = do
+    primStream <- toPrimitiveStream getPrimArr
+    fragStream <- rasterize getRastOpt $ vertShader <$> primStream
+    drawWindowColor getDrawOpt $ fragShader <$> fragStream
+--  = drawWindowColor getDrawOpt . fmap fragShader -- Alternate way to write the same code, using a compositional style.
+--  =<< rasterize getRastOpt . fmap vertShader
+--  =<< toPrimitiveStream getPrimArr
+  where
+    vertShader pos = (pos, ()) -- no input to be interpolated by fragment shader
+    fragShader () = V4 1 1 1 1 -- hardcoded color & color-format
 
-stream :: PrimitiveStream Triangle (Vec4 (Vertex Float))
-stream = toGPUStream TriangleList $
-    [   0.75  :.   0.75  :.  0.00  :.  1.00  :.  ()
-    ,   0.75  :. (-0.75) :.  0.00  :.  1.00  :.  ()
-    , (-0.75) :. (-0.75) :.  0.00  :.  1.00  :.  ()
-    ]
-
-vs :: Vec4 (Vertex Float) -> (Vec4 (Vertex Float), ())
-vs pos = (pos, ())
-
-fs :: () -> Color RGBFormat (Fragment Float)
-fs _ = RGB $ vec 1
-
--- eof
+display
+    :: Window os RGBAFloat ()
+    -> Buffer os (B4 Float)
+    -> CompiledShader os (ShaderEnv os Triangles (B4 Float) RGBAFloat ())
+    -> ContextT Handle os IO ()
+display win vertexBuffer shaderProg = do
+    Just (x, y) <- GLFW.getWindowSize win -- whereas gltut uses a reshape callback
+    render $ do
+        clearWindowColor win (V4 0 0 0 0)
+        vertexArray <- newVertexArray vertexBuffer
+        shaderProg $ ShaderEnv
+            { getPrimArr = toPrimitiveArray TriangleList vertexArray
+            , getRastOpt = (FrontAndBack, ViewPort (V2 0 0) (V2 x y), DepthRange 0 1)
+            , getDrawOpt = (win, ContextColorOption NoBlending (V4 True True True True))
+            }
+    swapWindowBuffers win
---
+++ default.nix
@@ -0,0 +1,22 @@
+{ config ? { /*allowBroken = true;*/ }, ... }:
+let
+  gpipe-glfw-src = (import <nixpkgs> { }).fetchFromGitHub {
+    owner = "plredmond";
+    repo = "GPipe-GLFW";
+    rev = "d9a43ec";
+    sha256 = "0xj6ny0n9yw35m80495rjln22fn5szzj7w69911fsm4b0n0k05fk";
+  };
+  # extract pinned nixpkgs and haskellPackages
+  elsewhere = import gpipe-glfw-src { inherit config; mkEnv = false; };
+  nixpkgs = elsewhere.nixpkgs;
+  haskellPackages = elsewhere.haskellPackages;
+  # define the derivation and the environment
+  projectPackages = with haskellPackages; [
+    (callCabal2nix "tut01" ./Tut_01_Hello_Triangle/hs { })
+    #(callCabal2nix "framework" ./framework/hs { })
+  ];
+  drv = nixpkgs.buildEnv { name = "gltut-project"; paths = projectPackages; };
+  env = ((builtins.head projectPackages).envFunc { withHoogle = true; }).overrideAttrs
+    (old: { nativeBuildInputs = old.nativeBuildInputs ++ [ nixpkgs.ghcid ]; });
+in
+if nixpkgs.lib.inNixShell then env else drv
