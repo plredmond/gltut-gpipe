e6ce7f3f3a1ee9c0efe455d04a4877dc9d6fc318
PLR <51248199+plredmond@users.noreply.github.com>
2020-11-05 22:23:10 -0800

    port tut02 VertexColors

--- Tut 02 Playing with Colors/hs/VertexColors.hs
+++ Tut 02 Playing with Colors/hs/VertexColors.hs
@@ -1,44 +1,77 @@
-import qualified Graphics.GLTut.Framework as Framework
-import qualified Graphics.UI.GLUT as GLUT
+{-# LANGUAGE LambdaCase, NegativeLiterals #-} -- syntax niceties
+{-# LANGUAGE TypeFamilies #-} -- gpipe requirements
 
-import Graphics.GPipe
-import Data.Vec as V
+import Control.Monad.IO.Class (liftIO)
+import System.Environment (getProgName)
+import qualified Control.Concurrent.MVar as MVar
+
+import Graphics.GPipe -- unqualified
+import Graphics.GPipe.Context.GLFW (Handle)
+import qualified Graphics.GPipe.Context.GLFW as GLFW
 
 main :: IO ()
-main = Framework.main keyboard displayIO (\_ -> return ())
+main = runContextT GLFW.defaultHandleConfig $ do
+    -- make a window
+    win <- newWindow (WindowFormatColor RGBA8) . GLFW.defaultWindowConfig =<< liftIO getProgName
+    -- hook up to receive ESC key and window-close events
+    close <- liftIO $ MVar.newEmptyMVar
+    _ <- GLFW.setWindowCloseCallback win . Just $
+        MVar.tryPutMVar close "window closed" >> return ()
+    _ <- GLFW.setKeyCallback win . Just $ \k _ ks _ -> case (k, ks) of
+        (GLFW.Key'Escape, GLFW.KeyState'Pressed) -> MVar.tryPutMVar close "escape key" >> return ()
+        _ -> return ()
+    -- initializeProgram
+    prog <- compileShader shaderCode
+    buff <- initializeVertexBuffer
+    -- framework
+    loop close win buff prog
+  where
+    loop close win buff prog = (liftIO $ MVar.tryReadMVar close) >>= \case
+        Just msg -> liftIO . putStrLn $ "stopping because: " ++ msg
+        Nothing -> display win buff prog >> loop close win buff prog
 
--- Handle keyboard events.
-keyboard :: Char -> GLUT.Position -> IO ()
-keyboard '\ESC' _ = do GLUT.leaveMainLoop
-keyboard _      _ = do return ()
+initializeVertexBuffer :: ContextT Handle os IO (Buffer os (B4 Float))
+initializeVertexBuffer = do
+    vertexBufferObject <- newBuffer $ length vertexData
+    writeBuffer vertexBufferObject 0 vertexData
+    return vertexBufferObject
+  where
+    vertexData =
+        -- position data
+        [ V4  0.0  0.5   0 1
+        , V4  0.5 -0.366 0 1
+        , V4 -0.5 -0.366 0 1
+        -- color data
+        , V4  1.0  0.0   0 1
+        , V4  0.0  1.0   0 1
+        , V4  0.0  0.0   1 1
+        ]
 
--- Perform IO on behalf of display. Call display to produce the framebuffer.
-displayIO :: Vec2 Int -> IO (FrameBuffer RGBFormat () ())
-displayIO size = do
-    return $ display size
+type ShaderEnv os prim = (Window os RGBAFloat (), PrimitiveArray prim (B4 Float, B4 Float), V2 Int)
+shaderCode :: Shader os (ShaderEnv os prim) ()
+shaderCode = do
+    primStream <- toPrimitiveStream getPrimArr
+    fragStream <- rasterize getRastOpt $ fmap vertShader primStream
+    drawWindowColor getDrawOpt $ fmap fragShader fragStream
+  where
+    getPrimArr (_, arr, _) = arr
+    getRastOpt (_, _, siz) = (FrontAndBack, ViewPort (V2 0 0) siz, DepthRange 0 1)
+    getDrawOpt (win, _, _) = (win, ContextColorOption NoBlending (V4 True True True True))
+    vertShader (pos, col) = (pos, col) -- this could be `id`
+    fragShader col = col -- this could be `id`
 
--- Combine scene elements on a framebuffer.
-display :: Vec2 Int -> FrameBuffer RGBFormat () ()
-display _ = draw fragments cleared
-    where
-        draw = paintColor NoBlending (RGB $ vec True)
-        cleared = newFrameBufferColor (RGB $ vec 0)
-        fragments = fmap fs
-                  $ rasterizeBack
-                  $ fmap vs
-                  stream
-
-stream :: PrimitiveStream Triangle (Vec4 (Vertex Float), Vec4 (Vertex Float))
-stream = toGPUStream TriangleList $
-    [ (  0   :.  0.5   :.0:.1:.(), 1:.0:.0:.1:.())
-    , (  0.5 :.(-0.366):.0:.1:.(), 0:.1:.0:.1:.())
-    , ((-0.5):.(-0.366):.0:.1:.(), 0:.0:.1:.1:.())
-    ]
-
-vs :: (Vec4 (Vertex Float), Vec4 (Vertex Float)) -> (Vec4 (Vertex Float), Vec4 (Vertex Float))
-vs = id
-
-fs :: Vec4 (Fragment Float) -> Color RGBFormat (Fragment Float)
-fs col = RGB (V.take n3 col)
-
--- eof
+display
+    :: Window os RGBAFloat ()
+    -> Buffer os (B4 Float)
+    -> CompiledShader os (ShaderEnv os Triangles)
+    -> ContextT Handle os IO ()
+display win vertexBuffer shaderProg = do
+    Just (x, y) <- GLFW.getWindowSize win -- whereas gltut uses a reshape callback
+    render $ do
+        clearWindowColor win (V4 0 0 0 0)
+        vertexArray <- newVertexArray vertexBuffer
+        let posColArr = zipVertices (,)
+                (takeVertices 3 vertexArray)
+                (dropVertices 3 vertexArray :: VertexArray () (B4 Float))
+        shaderProg (win, toPrimitiveArray TriangleList posColArr, V2 x y)
+    swapWindowBuffers win
