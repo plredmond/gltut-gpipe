ac0bdf8d6ac1e6ad3fe145493c0c6baf6bccf29f
PLR <51248199+plredmond@users.noreply.github.com>
2020-11-07 21:04:13 -0800

    WIP reoganize files for tut04

--- Tut 04 Objects at Rest/hs/lib/Common.hs
+++ Tut 04 Objects at Rest/hs/lib/Common.hs
@@ -1,3 +1,22 @@
+module Graphics.GLTut.Tut04.Models
+( load_cube
+, PrimStream
+) where
+
+import qualified Graphics.GLTut.VecFile as VecFile
+import qualified Paths_gltut_tut04 as Paths
+
+import Graphics.GPipe (PrimitiveStream(), Triangle(TriangleList), Vertex())
+import Data.Vec (Vec4())
+
+type PrimStream = PrimitiveStream Triangle ( Vec4 (Vertex Float)
+                                           , Vec4 (Vertex Float) )
+
+load_cube :: IO PrimStream
+load_cube = do
+    p <- Paths.getDataFileName "cube.vec4"
+    VecFile.loadStream TriangleList p []
+
 module Graphics.GLTut.Tut04.VarProjectionPlane
 ( vpp
 ) where
