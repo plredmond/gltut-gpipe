470364777849aa83776721eac255962acf874e52
PLR <51248199+plredmond@users.noreply.github.com>
2020-11-08 16:59:01 -0800

    move framework to a dependency in the shell env; prefix all packages with 'gltut' again

--- default.nix
+++ default.nix
@@ -9,13 +9,22 @@ let
   # extract pinned nixpkgs and haskellPackages
   elsewhere = import gpipe-glfw-src { inherit config; mkEnv = false; };
   nixpkgs = elsewhere.nixpkgs;
-  haskellPackages = elsewhere.haskellPackages;
+  # helper function to ignore gitignored files in source dirs
+  git-ignore = path: nixpkgs.nix-gitignore.gitignoreSource [ ] path;
+  # additively override haskellPackages to include gltut-framework
+  haskellPackages = elsewhere.haskellPackages.override
+    (old: {
+      overrides = self: super: with nixpkgs.haskell.lib;
+        (old.overrides self super) // {
+          gltut-framework = self.callCabal2nix "gltut-framework" (git-ignore ./framework/hs) { };
+        };
+    });
   # define the derivation and the environment
   projectPackages = with haskellPackages; [
-    (callCabal2nix "tut01" ./Tut_01/hs { })
-    (callCabal2nix "tut02" ./Tut_02/hs { })
-    (callCabal2nix "tut03" ./Tut_03/hs { })
-    (callCabal2nix "framework" ./framework/hs { })
+    (callCabal2nix "gltut-tut01" (git-ignore ./Tut_01/hs) { })
+    (callCabal2nix "gltut-tut02" (git-ignore ./Tut_02/hs) { })
+    (callCabal2nix "gltut-tut03" (git-ignore ./Tut_03/hs) { })
+    (callCabal2nix "gltut-tut04" (git-ignore ./Tut_04/hs) { })
   ];
   drv = nixpkgs.buildEnv { name = "gltut-project"; paths = projectPackages; };
   env = ((builtins.head projectPackages).envFunc { withHoogle = true; }).overrideAttrs
