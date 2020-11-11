b1feb466bff55563c97e6ab6ccd2a118bdf9cd5b
PLR <51248199+plredmond@users.noreply.github.com>
2020-11-11 14:52:09 -0800

    port tut03 vertPositionOffset to gltut-framework main

--- Tut 03 OpenGLs Moving Triangle/hs/vertPositionOffset.hs
+++ Tut 03 OpenGLs Moving Triangle/hs/vertPositionOffset.hs
@@ -2,57 +2,68 @@
 {-# LANGUAGE TypeFamilies #-} -- gpipe requirements
 
 import Control.Monad.IO.Class (liftIO)
-import System.Environment (getProgName)
-import qualified Control.Concurrent.MVar as MVar
 import qualified Control.Lens as Lens
 
 import Graphics.GPipe -- unqualified
 import Graphics.GPipe.Context.GLFW (Handle)
 import qualified Graphics.GPipe.Context.GLFW as GLFW
+import qualified Graphics.GLTut.Framework as FW
 
 main :: IO ()
 main = runContextT GLFW.defaultHandleConfig $ do
-    -- make a window
-    win <- newWindow (WindowFormatColor RGBA8) . GLFW.defaultWindowConfig =<< liftIO getProgName
-    -- hook up to receive ESC key and window-close events
-    close <- liftIO $ MVar.newEmptyMVar
-    _ <- GLFW.setWindowCloseCallback win . Just $
-        MVar.tryPutMVar close "window closed" >> return ()
-    _ <- GLFW.setKeyCallback win . Just $ \k _ ks _ -> case (k, ks) of
-        (GLFW.Key'Escape, GLFW.KeyState'Pressed) -> MVar.tryPutMVar close "escape key" >> return ()
-        _ -> return ()
-    -- initializeProgram
-    prog <- compileShader shaderCode
-    buff <- initializeVertexBuffer
-    unif <- newBuffer 1
-    -- framework
-    loop close win unif buff prog
-  where
-    loop close win unif buff prog = (liftIO $ MVar.tryReadMVar close) >>= \case
-        Just msg -> liftIO . putStrLn $ "stopping because: " ++ msg
-        Nothing -> display win unif buff prog >> loop close win unif buff prog
+    _ <- FW.main (WindowFormatColor RGBA8) initialize display keyboard reshape
+    return ()
 
-initializeVertexBuffer :: ContextT Handle os IO (Buffer os (B4 Float))
-initializeVertexBuffer = do
-    positionBufferObject <- newBuffer $ length vertexPositions
-    writeBuffer positionBufferObject 0 vertexPositions
-    return positionBufferObject
-
-vertexPositions :: [V4 Float]
-vertexPositions =
-    [ V4  0.25  0.25 0 1
-    , V4  0.25 -0.25 0 1
-    , V4 -0.25 -0.25 0 1
-    ]
+-- | Unifs is the type of uniform buffers and offsets into them.
+type Unifs os = (Buffer os (Uniform (B2 Float)), BufferStartPos)
 
 -- | This shader env became a bit bigger in this tutoral. It's starting to be
 -- ungainly. In the next tutorial it'll be converted to a datatype.
 type ShaderEnv os =
-    ( Window os RGBAFloat ()
-    , (Buffer os (Uniform (B2 Float)), Int) -- Offsets uniform
-    , PrimitiveArray Triangles (B4 Float) -- Vertex buffer
-    , V2 Int -- Viewport size
+    ( Unifs os
+    , PrimitiveArray Triangles (B4 Float)
+    , ViewPort
+    , Window os RGBAFloat ()
     )
+
+-- | Similarly, this resources env is getting too big and will become a
+-- datatype.
+type Env os =
+    ( Unifs os
+    , Buffer os (B4 Float)
+    , CompiledShader os (ShaderEnv os)
+    , ViewPort
+    )
+
+initialize :: Window os RGBAFloat () -> [String] -> ContextT Handle os IO (Env os)
+initialize _win _args = do
+    theProgram <- compileShader shaderCode
+    positionBufferObject <- newBuffer $ length vertexPositions
+    writeBuffer positionBufferObject 0 vertexPositions
+    offsetLocation <- newBuffer 1
+    let unifs = (offsetLocation, 0)
+    return (unifs, positionBufferObject, theProgram, ViewPort 0 0)
+
+display :: Window os RGBAFloat () -> Env os -> ContextT Handle os IO (Env os)
+display win env@(unifs, positionBufferObject, theProgram, viewport) = do
+    adjustOffsetUniform unifs
+    render $ do
+        clearWindowColor win 0
+        vertexArray <- newVertexArray positionBufferObject
+        theProgram
+            ( unifs
+            , toPrimitiveArray TriangleList vertexArray
+            , viewport
+            , win
+            )
+    swapWindowBuffers win
+    return env
+  where
+    adjustOffsetUniform (offsetLocation, _) = do
+        seconds <- liftIO GLFW.getTime
+        let offsets = computePositionOffsets $ maybe 0 realToFrac seconds
+        writeBuffer offsetLocation 0 [offsets]
+
 shaderCode :: Shader os (ShaderEnv os) ()
 shaderCode = do
     offsets <- getUniform getOffsets
@@ -64,35 +75,16 @@ shaderCode = do
   where
     -- In the next tutorial, these functions will become fields in the new
     -- shader env datatype.
-    getPrimArr (_, _, arr, _) = arr
-    getRastOpt (_, _, _, siz) = (FrontAndBack, ViewPort (V2 0 0) siz, DepthRange 0 1)
-    getDrawOpt (win, _, _, _) = (win, ContextColorOption NoBlending (V4 True True True True))
-    getOffsets (_, off, _, _) = off
+    getOffsets (off, _, _, _) = off
+    getPrimArr (_, arr, _, _) = arr
+    getRastOpt (_, _, vpt, _) = (FrontAndBack, vpt, DepthRange 0 1)
+    getDrawOpt (_, _, _, win) = (win, ContextColorOption NoBlending (pure True))
 
-display
-    :: Window os RGBAFloat ()
-    -> Buffer os (Uniform (B2 Float))
-    -> Buffer os (B4 Float)
-    -> CompiledShader os (ShaderEnv os)
-    -> ContextT Handle os IO ()
-display win offsetUnif vertexBuffer shaderProg = do
-    adjustOffsetUniform
-    Just (x, y) <- GLFW.getWindowSize win -- whereas gltut uses a reshape callback
-    render $ do
-        clearWindowColor win (V4 0 0 0 0)
-        vertexArray <- newVertexArray vertexBuffer
-        shaderProg
-            ( win
-            , (offsetUnif, 0)
-            , toPrimitiveArray TriangleList vertexArray
-            , V2 x y
-            )
-    swapWindowBuffers win
-  where
-    adjustOffsetUniform = do
-        seconds <- liftIO GLFW.getTime
-        let offsets = computePositionOffsets $ maybe 0 realToFrac seconds
-        writeBuffer offsetUnif 0 [offsets]
+keyboard :: Window os RGBAFloat () -> Env os -> GLFW.Key -> GLFW.KeyState -> GLFW.ModifierKeys -> ContextT Handle os IO (Env os)
+keyboard _win env _key _keyState _modKeys = return env
+
+reshape :: Window os RGBAFloat () -> Env os -> V2 Int -> ContextT Handle os IO (Env os)
+reshape _win (unifs, buff, prog, _) size = return (unifs, buff, prog, ViewPort 0 size)
 
 computePositionOffsets :: Real' a => a -> V2 a
 computePositionOffsets elapsedTime = V2
@@ -102,3 +94,10 @@ computePositionOffsets elapsedTime = V2
     loopDuration = 5
     scale = pi * 2 / loopDuration
     currTimeThroughLoop = mod'' elapsedTime loopDuration
+
+vertexPositions :: [V4 Float]
+vertexPositions =
+    [ V4  0.25  0.25 0 1
+    , V4  0.25 -0.25 0 1
+    , V4 -0.25 -0.25 0 1
+    ]
