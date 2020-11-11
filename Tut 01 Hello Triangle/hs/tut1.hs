07d562c014a7936c18d76b46c14b33685f67b422
PLR <51248199+plredmond@users.noreply.github.com>
2020-11-10 23:37:00 -0800

    port tut01 tut1 to gltut-framework main

--- Tut 01 Hello Triangle/hs/tut1.hs
+++ Tut 01 Hello Triangle/hs/tut1.hs
@@ -1,40 +1,31 @@
 {-# LANGUAGE LambdaCase, NegativeLiterals #-} -- syntax niceties
 {-# LANGUAGE TypeFamilies #-} -- gpipe requirements
 
-import Control.Monad.IO.Class (liftIO)
-import System.Environment (getProgName)
-import qualified Control.Concurrent.MVar as MVar
-
 import Graphics.GPipe -- unqualified
 import Graphics.GPipe.Context.GLFW (Handle)
 import qualified Graphics.GPipe.Context.GLFW as GLFW
+import qualified Graphics.GLTut.Framework as FW
 
 main :: IO ()
-main = runContextT GLFW.defaultHandleConfig $ do
-    -- make a window
-    win <- newWindow (WindowFormatColor RGBA8) . GLFW.defaultWindowConfig =<< liftIO getProgName
-    -- hook up to receive ESC key and window-close events
-    close <- liftIO $ MVar.newEmptyMVar
-    _ <- GLFW.setWindowCloseCallback win . Just $
-        MVar.tryPutMVar close "window closed" >> return ()
-    _ <- GLFW.setKeyCallback win . Just $ \k _ ks _ -> case (k, ks) of
-        (GLFW.Key'Escape, GLFW.KeyState'Pressed) -> MVar.tryPutMVar close "escape key" >> return ()
-        _ -> return ()
-    -- initializeProgram
-    prog <- compileShader shaderCode
-    buff <- initializeVertexBuffer
-    -- framework
-    loop close win buff prog
-  where
-    loop close win buff prog = (liftIO $ MVar.tryReadMVar close) >>= \case
-        Just msg -> liftIO . putStrLn $ "stopping because: " ++ msg
-        Nothing -> display win buff prog >> loop close win buff prog
+main = runContextT GLFW.defaultHandleConfig
+    $ FW.main (WindowFormatColor RGBA8) initialize display keyboard reshape
+    >> return ()
 
-initializeVertexBuffer :: ContextT Handle os IO (Buffer os (B4 Float))
-initializeVertexBuffer = do
+-- | ShaderEnv is the type of resources used by the shader code on the graphics
+-- card.
+type ShaderEnv os = (PrimitiveArray Triangles (B4 Float), ViewPort, Window os RGBAFloat ())
+
+-- | Env is the type of resources used by the display function and other
+-- callbacks in the application.
+type Env os = (Buffer os (B4 Float), CompiledShader os (ShaderEnv os), ViewPort)
+
+-- | Initialize the Env.
+initialize :: Window os RGBAFloat () -> [String] -> ContextT Handle os IO (Env os)
+initialize _win _args = do
+    theProgram <- compileShader shaderCode
     positionBufferObject <- newBuffer $ length vertexPositions
     writeBuffer positionBufferObject 0 vertexPositions
-    return positionBufferObject
+    return (positionBufferObject, theProgram, ViewPort 0 0)
   where
     vertexPositions =
         [ V4  0.75  0.75 0 1
@@ -42,33 +33,34 @@ initializeVertexBuffer = do
         , V4 -0.75 -0.75 0 1
         ]
 
--- | Somewhat minimal shader which gets the (single) vertex array as a
--- primitive stream, maps a noop vertex-shader over it, rasterizes to a
--- fragment stream, maps the noop fragment-shader over it, and draws to the
--- window specified by the environment.
-type ShaderEnv os = (Window os RGBAFloat (), PrimitiveArray Triangles (B4 Float), V2 Int)
+display :: Window os RGBAFloat () -> Env os -> ContextT Handle os IO (Env os)
+display win st@(positionBufferObject, theProgram, viewPort) = do
+    render $ do
+        clearWindowColor win (V4 0 0 0 0)
+        vertexArray <- newVertexArray positionBufferObject
+        theProgram
+            (toPrimitiveArray TriangleList vertexArray, viewPort, win)
+    swapWindowBuffers win
+    return st
+
+-- | Shader code which (1) gets the vertex-array as a primitive-stream, (2)
+-- maps a noop vertex-shader over it, (3) rasterizes to a fragment-stream, (4)
+-- maps the noop fragment-shader over it, and (5) draws to the specified
+-- window.
 shaderCode :: Shader os (ShaderEnv os) ()
 shaderCode = do
     primStream <- toPrimitiveStream getPrimArr
     fragStream <- rasterize getRastOpt $ fmap vertShader primStream
     drawWindowColor getDrawOpt $ fmap fragShader fragStream
   where
-    getPrimArr (_, arr, _) = arr
-    getRastOpt (_, _, siz) = (FrontAndBack, ViewPort (V2 0 0) siz, DepthRange 0 1)
-    getDrawOpt (win, _, _) = (win, ContextColorOption NoBlending (V4 True True True True))
+    getPrimArr (arr, _, _) = arr
+    getRastOpt (_, vpt, _) = (FrontAndBack, vpt, DepthRange 0 1)
+    getDrawOpt (_, _, win) = (win, ContextColorOption NoBlending (pure True))
     vertShader pos = (pos, ()) -- no input to be interpolated by fragment shader
     fragShader () = V4 1 1 1 1 -- hardcoded color & color-format
 
-display
-    :: Window os RGBAFloat ()
-    -> Buffer os (B4 Float)
-    -> CompiledShader os (ShaderEnv os)
-    -> ContextT Handle os IO ()
-display win vertexBuffer shaderProg = do
-    Just (x, y) <- GLFW.getWindowSize win -- whereas gltut uses a reshape callback
-    render $ do
-        clearWindowColor win (V4 0 0 0 0)
-        vertexArray <- newVertexArray vertexBuffer
-        shaderProg
-            (win, toPrimitiveArray TriangleList vertexArray, V2 x y)
-    swapWindowBuffers win
+keyboard :: Window os RGBAFloat () -> Env os -> GLFW.Key -> GLFW.KeyState -> GLFW.ModifierKeys -> ContextT Handle os IO (Env os)
+keyboard _win st _key _keyState _modKeys = return st
+
+reshape :: Window os RGBAFloat () -> Env os -> V2 Int -> ContextT Handle os IO (Env os)
+reshape _win (buff, prog, _) size = return (buff, prog, ViewPort 0 size)
