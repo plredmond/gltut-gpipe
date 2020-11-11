0904c2f3c854e774a69e7c038d4c6e4bf9a1a2c6
PLR <51248199+plredmond@users.noreply.github.com>
2020-11-11 14:28:29 -0800

    port tut03 cpuPositionOffset to gltut-framework main

--- Tut 03 OpenGLs Moving Triangle/hs/cpuPositionOffset.hs
+++ Tut 03 OpenGLs Moving Triangle/hs/cpuPositionOffset.hs
@@ -2,81 +2,62 @@
 {-# LANGUAGE TypeFamilies #-} -- gpipe requirements
 
 import Control.Monad.IO.Class (liftIO)
-import System.Environment (getProgName)
-import qualified Control.Concurrent.MVar as MVar
 import qualified Control.Lens as Lens
 
 import Graphics.GPipe -- unqualified
 import Graphics.GPipe.Context.GLFW (Handle)
 import qualified Graphics.GPipe.Context.GLFW as GLFW
+import qualified Graphics.GLTut.Framework as FW
 
 main :: IO ()
 main = runContextT GLFW.defaultHandleConfig $ do
-    -- make a window
-    win <- newWindow (WindowFormatColor RGBA8) . GLFW.defaultWindowConfig =<< liftIO getProgName
-    -- hook up to receive ESC key and window-close events
-    close <- liftIO $ MVar.newEmptyMVar
-    _ <- GLFW.setWindowCloseCallback win . Just $
-        MVar.tryPutMVar close "window closed" >> return ()
-    _ <- GLFW.setKeyCallback win . Just $ \k _ ks _ -> case (k, ks) of
-        (GLFW.Key'Escape, GLFW.KeyState'Pressed) -> MVar.tryPutMVar close "escape key" >> return ()
-        _ -> return ()
-    -- initializeProgram
-    prog <- compileShader shaderCode
-    buff <- initializeVertexBuffer
-    -- framework
-    loop close win buff prog
-  where
-    loop close win buff prog = (liftIO $ MVar.tryReadMVar close) >>= \case
-        Just msg -> liftIO . putStrLn $ "stopping because: " ++ msg
-        Nothing -> display win buff prog >> loop close win buff prog
+    _ <- FW.main (WindowFormatColor RGBA8) initialize display keyboard reshape
+    return ()
 
-initializeVertexBuffer :: ContextT Handle os IO (Buffer os (B4 Float))
-initializeVertexBuffer = do
+type ShaderEnv os = (PrimitiveArray Triangles (B4 Float), ViewPort, Window os RGBAFloat ())
+
+type Env os = (Buffer os (B4 Float), CompiledShader os (ShaderEnv os), ViewPort)
+
+initialize :: Window os RGBAFloat () -> [String] -> ContextT Handle os IO (Env os)
+initialize _win _args = do
+    theProgram <- compileShader shaderCode
     positionBufferObject <- newBuffer $ length vertexPositions
     writeBuffer positionBufferObject 0 vertexPositions
-    return positionBufferObject
+    return (positionBufferObject, theProgram, ViewPort 0 0)
 
-vertexPositions :: [V4 Float]
-vertexPositions =
-    [ V4  0.25  0.25 0 1
-    , V4  0.25 -0.25 0 1
-    , V4 -0.25 -0.25 0 1
-    ]
-
-type ShaderEnv os =
-    (Window os RGBAFloat (), PrimitiveArray Triangles (B4 Float), V2 Int)
-shaderCode :: Shader os (ShaderEnv os) ()
-shaderCode = do
-    primStream <- toPrimitiveStream getPrimArr
-    fragStream <- rasterize getRastOpt $ fmap vertShader primStream
-    drawWindowColor getDrawOpt $ fmap fragShader fragStream
-  where
-    getPrimArr (_, arr, _) = arr
-    getRastOpt (_, _, siz) = (FrontAndBack, ViewPort (V2 0 0) siz, DepthRange 0 1)
-    getDrawOpt (win, _, _) = (win, ContextColorOption NoBlending (V4 True True True True))
-    vertShader pos = (pos, ()) -- no input to be interpolated by fragment shader
-    fragShader () = 1
-
-display
-    :: Window os RGBAFloat ()
-    -> Buffer os (B4 Float)
-    -> CompiledShader os (ShaderEnv os)
-    -> ContextT Handle os IO ()
-display win vertexBuffer shaderProg = do
+display :: Window os RGBAFloat () -> Env os -> ContextT Handle os IO (Env os)
+display win env@(positionBufferObject, theProgram, viewport) = do
     adjustVertexData
-    Just (x, y) <- GLFW.getWindowSize win -- whereas gltut uses a reshape callback
     render $ do
-        clearWindowColor win (V4 0 0 0 0)
-        vertexArray <- newVertexArray vertexBuffer
-        shaderProg
-            (win, toPrimitiveArray TriangleList vertexArray, V2 x y)
+        clearWindowColor win 0
+        vertexArray <- newVertexArray positionBufferObject
+        theProgram
+            (toPrimitiveArray TriangleList vertexArray, viewport, win)
     swapWindowBuffers win
+    return env
   where
     adjustVertexData = do
         seconds <- liftIO GLFW.getTime
         let offsets = computePositionOffsets $ maybe 0 realToFrac seconds
-        writeBuffer vertexBuffer 0 $ fmap (Lens.over _xy (+offsets)) vertexPositions
+        writeBuffer positionBufferObject 0 $ fmap (Lens.over _xy (+offsets)) vertexPositions
+
+shaderCode :: Shader os (ShaderEnv os) ()
+shaderCode = do
+    let vertShader pos = (pos, ()) -- no input to be interpolated by fragment shader
+        fragShader () = 1 -- hardcoded color & inferred color-format
+    primStream <- toPrimitiveStream getPrimArr
+    fragStream <- rasterize getRastOpt $ fmap vertShader primStream
+    drawWindowColor getDrawOpt $ fmap fragShader fragStream
+  where
+    getPrimArr (arr, _, _) = arr
+    getRastOpt (_, vpt, _) = (FrontAndBack, vpt, DepthRange 0 1)
+    getDrawOpt (_, _, win) = (win, ContextColorOption NoBlending (pure True))
+
+keyboard :: Window os RGBAFloat () -> Env os -> GLFW.Key -> GLFW.KeyState -> GLFW.ModifierKeys -> ContextT Handle os IO (Env os)
+keyboard _win env _key _keyState _modKeys = return env
+
+reshape :: Window os RGBAFloat () -> Env os -> V2 Int -> ContextT Handle os IO (Env os)
+reshape _win (buff, prog, _) size = return (buff, prog, ViewPort 0 size)
 
 computePositionOffsets :: Real' a => a -> V2 a
 computePositionOffsets elapsedTime = V2
@@ -86,3 +67,10 @@ computePositionOffsets elapsedTime = V2
     loopDuration = 5
     scale = pi * 2 / loopDuration
     currTimeThroughLoop = mod'' elapsedTime loopDuration
+
+vertexPositions :: [V4 Float]
+vertexPositions =
+    [ V4  0.25  0.25 0 1
+    , V4  0.25 -0.25 0 1
+    , V4 -0.25 -0.25 0 1
+    ]
