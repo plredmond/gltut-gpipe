4ee4df3796773790f71a436fd2be80adc82e0f2e
PLR <51248199+plredmond@users.noreply.github.com>
2020-11-11 21:40:50 -0800

    align tut04 aspectRatio with tut04 matrixPerspective

--- Tut 04 Objects at Rest/hs/aspectRatio.hs
+++ Tut 04 Objects at Rest/hs/aspectRatio.hs
@@ -9,9 +9,9 @@ import qualified Graphics.GPipe.Context.GLFW as GLFW
 import qualified Graphics.GLTut.Framework as FW
 
 main :: IO ()
-main = runContextT GLFW.defaultHandleConfig
-    $ FW.main (WindowFormatColor RGBA8) initialize display keyboard reshape
-    >> return ()
+main = runContextT GLFW.defaultHandleConfig $ do
+    _ <- FW.main (WindowFormatColor RGBA8) initialize display keyboard reshape
+    return ()
 
 data Unifs os = Unifs
     { offsetUniform :: (Buffer os (Uniform (B2 Float)), BufferStartPos)
@@ -28,8 +28,8 @@ data ShaderEnv os = ShaderEnv
 data Env os = Env
     { vertexBuff :: Buffer os (B4 Float)
     , shaderProg :: CompiledShader os (ShaderEnv os)
-    , uniforms :: Unifs os
     , viewport :: ViewPort
+    , uniforms :: Unifs os
     }
 
 initialize :: Window os RGBAFloat () -> [String] -> ContextT Handle os IO (Env os)
@@ -38,31 +38,34 @@ initialize _win _args = do
     vertexBufferObject <- newBuffer $ length vertexData
     writeBuffer vertexBufferObject 0 vertexData
     unifs <- do
-        unifV2 <- newBuffer 1
-        -- The M44 uniform isn't initialized until `display` is called.
+        -- This offset is adjusted from the previous tutorial such that here in
+        -- "AspectRatio.cpp" the object is off the side of the screen, and you
+        -- must resize to test the constant aspect ratio.
+        unifB2F <- newBuffer 1
+        writeBuffer unifB2F offsetUniformOfs [V2 1.5 0.5]
+        -- The M44 uniform isn't initialized until `reshape` is called.
         unifM44 <- newBuffer 1
-        -- This is adjusted in "AspectRatio.cpp" from the previous tutorial to
-        -- put the object off the side of the screen, so that you must resize
-        -- to test the constant aspect ratio.
-        writeBuffer unifV2 0 [V2 1.5 0.5]
         return Unifs
-            { offsetUniform = (unifV2, 0)
-            , perspectiveMatrixUnif = (unifM44, 0)
+            { offsetUniform = (unifB2F, offsetUniformOfs)
+            , perspectiveMatrixUnif = (unifM44, perspectiveMatrixUnifOfs)
             }
-    return $ Env vertexBufferObject theProgram unifs (ViewPort 0 0)
+    return $ Env vertexBufferObject theProgram (ViewPort 0 0) unifs
+  where
+    offsetUniformOfs = 0
+    perspectiveMatrixUnifOfs = 0
 
 display :: Window os RGBAFloat () -> Env os -> ContextT Handle os IO (Env os)
-display win env@Env{vertexBuff, shaderProg, uniforms, viewport} = do
+display win env = do
     render $ do
         clearWindowColor win 0
-        vertexArray <- newVertexArray vertexBuff
-        shaderProg ShaderEnv
-            { getUnifs = uniforms
+        vertexArray <- newVertexArray $ vertexBuff env
+        (shaderProg env) ShaderEnv
+            { getUnifs = uniforms env
             , getPrimArr = toPrimitiveArray TriangleList $ zipVertices (,)
                 (takeVertices (length vertexData `div` 2) vertexArray)
                 (dropVertices (length vertexData `div` 2) vertexArray :: VertexArray () (B4 Float))
-            , getRastOpt = (Back, viewport, DepthRange 0 1)
-            , getDrawOpt = (win, ContextColorOption NoBlending $ pure True)
+            , getRastOpt = (Back, viewport env, DepthRange 0 1)
+            , getDrawOpt = (win, ContextColorOption NoBlending (pure True))
             }
     swapWindowBuffers win
     return env
@@ -88,7 +91,7 @@ reshape _win env@Env{uniforms} size = do
     return env{viewport=ViewPort 0 size}
 
 updatePerspectiveMatrix :: V2 Int -> Unifs os -> ContextT Handle os IO ()
-updatePerspectiveMatrix (V2 width height) Unifs{perspectiveMatrixUnif=(unifM44,buffPos)} = do
+updatePerspectiveMatrix (V2 width height) Unifs{perspectiveMatrixUnif=(buff, buffPos)} = do
     let frustrumScale = 1
         zNear = 0.5
         zFar = 3
@@ -96,7 +99,7 @@ updatePerspectiveMatrix (V2 width height) Unifs{perspectiveMatrixUnif=(unifM44,b
         y = frustrumScale
         z = (zFar + zNear) / (zNear - zFar)
         w = (2 * zFar * zNear) / (zNear - zFar)
-    writeBuffer unifM44 buffPos
+    writeBuffer buff buffPos
         [V4 (V4 x 0  0 0)
             (V4 0 y  0 0)
             (V4 0 0  z w)
