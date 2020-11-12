c61957443b9829256d58fff0e82bf7743d3aa462
PLR <51248199+plredmond@users.noreply.github.com>
2020-11-11 16:12:11 -0800

    port tut03 vertCalcOffset to gltut-framework main

--- Tut 03 OpenGLs Moving Triangle/hs/vertCalcOffset.hs
+++ Tut 03 OpenGLs Moving Triangle/hs/vertCalcOffset.hs
@@ -2,104 +2,101 @@
 {-# LANGUAGE TypeFamilies #-} -- gpipe requirements
 
 import Control.Monad.IO.Class (liftIO)
-import System.Environment (getProgName)
-import qualified Control.Concurrent.MVar as MVar
 import qualified Control.Lens as Lens
 
 import Graphics.GPipe -- unqualified
 import Graphics.GPipe.Context.GLFW (Handle)
 import qualified Graphics.GPipe.Context.GLFW as GLFW
+import qualified Graphics.GLTut.Framework as FW
 
 main :: IO ()
 main = runContextT GLFW.defaultHandleConfig $ do
-    -- make a window
-    win <- newWindow (WindowFormatColor RGBA8) . GLFW.defaultWindowConfig =<< liftIO getProgName
-    -- hook up to receive ESC key and window-close events
-    close <- liftIO $ MVar.newEmptyMVar
-    _ <- GLFW.setWindowCloseCallback win . Just $
-        MVar.tryPutMVar close "window closed" >> return ()
-    _ <- GLFW.setKeyCallback win . Just $ \k _ ks _ -> case (k, ks) of
-        (GLFW.Key'Escape, GLFW.KeyState'Pressed) -> MVar.tryPutMVar close "escape key" >> return ()
-        _ -> return ()
-    -- initializeProgram
-    prog <- compileShader shaderCode
-    buff <- initializeVertexBuffer
-    unif <- newBuffer 2 -- We'll store both the loop-duration and elapsed-seconds in one uniform buffer.
-    writeBuffer unif loopDurUnifOffset [5]
-    -- framework
-    loop close win unif buff prog
-  where
-    loop close win unif buff prog = (liftIO $ MVar.tryReadMVar close) >>= \case
-        Just msg -> liftIO . putStrLn $ "stopping because: " ++ msg
-        Nothing -> display win unif buff prog >> loop close win unif buff prog
+    _ <- FW.main (WindowFormatColor RGBA8) initialize display keyboard reshape
+    return ()
 
-loopDurUnifOffset :: BufferStartPos
-loopDurUnifOffset = 0
+-- | Unifs is now a datatype because we're juggling mulitple uniforms.
+data Unifs os = Unifs
+    { elapsedSecUniform :: (Buffer os (Uniform (B Float)), BufferStartPos)
+    , loopDurationUnf :: (Buffer os (Uniform (B Float)), BufferStartPos)
+    }
 
-secondsUnifOffset :: BufferStartPos
-secondsUnifOffset = 1
-
-initializeVertexBuffer :: ContextT Handle os IO (Buffer os (B4 Float))
-initializeVertexBuffer = do
-    positionBufferObject <- newBuffer $ length vertexPositions
-    writeBuffer positionBufferObject 0 vertexPositions
-    return positionBufferObject
-
-vertexPositions :: [V4 Float]
-vertexPositions =
-    [ V4  0.25  0.25 0 1
-    , V4  0.25 -0.25 0 1
-    , V4 -0.25 -0.25 0 1
-    ]
-
--- | The new shader env datatype is mostly a noop change. The latter three
--- fields in this env datatype have the names and types as the functions they
--- replaced. The first two fields are new, and they facilitate getting a
--- uniform value. Some options previously hardcoded into the shader are instead
--- hardcoded into the display function and passed to the shader via this env
--- datatype.
+-- | The new shader env datatype is mostly a noop change. The fields in this
+-- env datatype have the same names and types as the functions they replaced.
+-- Some options previously hardcoded into the shader are instead hardcoded into
+-- the display function and passed to the shader via this env datatype.
 data ShaderEnv os = ShaderEnv
-    { getLoopDur :: (Buffer os (Uniform (B Float)), Int)
-    , getSeconds :: (Buffer os (Uniform (B Float)), Int)
+    { getUnifs :: Unifs os
     , getPrimArr :: PrimitiveArray Triangles (B4 Float)
     , getRastOpt :: (Side, ViewPort, DepthRange)
     , getDrawOpt :: (Window os RGBAFloat (), ContextColorOption RGBAFloat)
     }
+
+-- | The new resources env datatype means the framework callbacks no longer
+-- pack and unpack tuples. When you write a GPipe program from scratch, this
+-- probably isn't necessary. Also note, the names of the fields here don't
+-- match the global variables in the cpp code.
+data Env os = Env
+    { vertexBuff :: Buffer os (B4 Float)
+    , shaderProg :: CompiledShader os (ShaderEnv os)
+    , viewport :: ViewPort
+    , uniforms :: Unifs os
+    }
+
+initialize :: Window os RGBAFloat () -> [String] -> ContextT Handle os IO (Env os)
+initialize _win _args = do
+    theProgram <- compileShader shaderCode
+    positionBufferObject <- newBuffer $ length vertexPositions
+    writeBuffer positionBufferObject 0 vertexPositions
+    unifs <- do
+        -- Since they have the same type, both the loop-duration and the
+        -- elapsed-seconds uniforms will be stored in the same buffer.
+        unifBF <- newBuffer 2
+        writeBuffer unifBF loopDurationUnfOfs [5]
+        -- The elapsed-seconds uniform will be written later.
+        return Unifs
+            { elapsedSecUniform = (unifBF, elapsedSecUniformOfs)
+            , loopDurationUnf = (unifBF, loopDurationUnfOfs)
+            }
+    return $ Env positionBufferObject theProgram (ViewPort 0 0) unifs
+  where
+    elapsedSecUniformOfs = 0
+    loopDurationUnfOfs = 1
+
+display :: Window os RGBAFloat () -> Env os -> ContextT Handle os IO (Env os)
+display win env = do
+    adjustSecondsUniform $ uniforms env
+    render $ do
+        clearWindowColor win 0
+        vertexArray <- newVertexArray $ vertexBuff env
+        (shaderProg env) ShaderEnv
+            { getUnifs = uniforms env
+            , getPrimArr = toPrimitiveArray TriangleList vertexArray
+            , getRastOpt = (FrontAndBack, viewport env, DepthRange 0 1)
+            , getDrawOpt = (win, ContextColorOption NoBlending (pure True))
+            }
+    swapWindowBuffers win
+    return env
+  where
+    adjustSecondsUniform Unifs{elapsedSecUniform=(buff, offset)} = do
+        seconds <- liftIO GLFW.getTime
+        writeBuffer buff offset [maybe 0 realToFrac seconds]
+
 shaderCode :: Shader os (ShaderEnv os) ()
 shaderCode = do
-    loopDur <- getUniform getLoopDur
-    seconds <- getUniform getSeconds
+    loopDur <- getUniform $ loopDurationUnf . getUnifs
+    seconds <- getUniform $ elapsedSecUniform . getUnifs
     let offsets = computePositionOffsets loopDur seconds
-        vertShader pos = (Lens.over _xy (+offsets) pos, ()) -- no input to be interpolated by fragment shader
+        vertShader pos = (Lens.over _xy (+ offsets) pos, ()) -- no input to be interpolated by fragment shader
         fragShader () = 1
     primStream <- toPrimitiveStream getPrimArr
     fragStream <- rasterize getRastOpt $ fmap vertShader primStream
     drawWindowColor getDrawOpt $ fmap fragShader fragStream
 
-display
-    :: Window os RGBAFloat ()
-    -> Buffer os (Uniform (B Float))
-    -> Buffer os (B4 Float)
-    -> CompiledShader os (ShaderEnv os)
-    -> ContextT Handle os IO ()
-display win singletonUnif vertexBuffer shaderProg = do
-    adjustSecondsUniform
-    Just (x, y) <- GLFW.getWindowSize win -- whereas gltut uses a reshape callback
-    render $ do
-        clearWindowColor win (V4 0 0 0 0)
-        vertexArray <- newVertexArray vertexBuffer
-        shaderProg ShaderEnv
-            { getLoopDur = (singletonUnif, loopDurUnifOffset)
-            , getSeconds = (singletonUnif, secondsUnifOffset)
-            , getPrimArr = toPrimitiveArray TriangleList vertexArray
-            , getRastOpt = (FrontAndBack, ViewPort (V2 0 0) (V2 x y), DepthRange 0 1)
-            , getDrawOpt = (win, ContextColorOption NoBlending (V4 True True True True))
-            }
-    swapWindowBuffers win
-  where
-    adjustSecondsUniform = do
-        seconds <- liftIO GLFW.getTime
-        writeBuffer singletonUnif secondsUnifOffset [maybe 0 realToFrac seconds]
+keyboard :: Window os RGBAFloat () -> Env os -> GLFW.Key -> GLFW.KeyState -> GLFW.ModifierKeys -> ContextT Handle os IO (Env os)
+keyboard _win env _key _keyState _modKeys = return env
+
+reshape :: Window os RGBAFloat () -> Env os -> V2 Int -> ContextT Handle os IO (Env os)
+reshape _win env size = return env{viewport=ViewPort 0 size}
 
 computePositionOffsets :: Real' a => a -> a -> V2 a
 computePositionOffsets loopDuration elapsedTime = V2
@@ -108,3 +105,10 @@ computePositionOffsets loopDuration elapsedTime = V2
   where
     scale = pi * 2 / loopDuration
     currTimeThroughLoop = mod'' elapsedTime loopDuration
+
+vertexPositions :: [V4 Float]
+vertexPositions =
+    [ V4  0.25  0.25 0 1
+    , V4  0.25 -0.25 0 1
+    , V4 -0.25 -0.25 0 1
+    ]
